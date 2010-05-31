b71eb623e2fa9b25407ab81e617e8b5775d1f163
Sebastian Bergmann <sb@sebastian-bergmann.de>
2010-05-31 14:29:45 +0200

    Add rename_function().
    Imported/Merged from http://github.com/scottmac/php-function-rename,
    donated by Facebook Inc.

--- test_helpers.c
+++ test_helpers.c
@@ -238,6 +238,81 @@ static PHP_FUNCTION(set_new_overload)
 }
 /* }}} */
 
+/* {{{ proto bool rename_function(string orig_func_name, string new_func_name)
+   Rename a function from its original to a new name. This is mainly useful in
+   unittest to stub out untested functions */
+PHP_FUNCTION(rename_function)
+{
+	zend_function *func, *dummy_func;
+	char *orig_fname, *new_fname, *lower_orig, *lower_new;
+	int orig_fname_len, new_fname_len, i;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "ss", &orig_fname, &orig_fname_len, &new_fname, &new_fname_len) == FAILURE) {
+		return;
+	}
+
+	// Normalize the source function name to lower case.
+	lower_orig = malloc(orig_fname_len+1);
+	for (i = 0; i < orig_fname_len; i ++) {
+		lower_orig[i] = tolower(orig_fname[i]);
+	}
+	lower_orig[i] = 0;
+
+	// Normalize the destination function name to lower case.
+	lower_new = malloc(new_fname_len + 1);
+	for (i = 0; i < new_fname_len; i++) {
+		lower_new[i] = tolower(new_fname[i]);
+	}
+	lower_new[i] = 0;
+
+	if (zend_hash_find(EG(function_table), lower_orig, orig_fname_len + 1, (void **) &func) == FAILURE) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s(%s, %s) failed: %s does not exist!"			,
+						get_active_function_name(TSRMLS_C),
+						lower_orig,  lower_new, lower_orig);
+		RETVAL_FALSE;
+		goto rename_end;
+	}
+
+	if (func->type != ZEND_USER_FUNCTION) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s(%s, %s) failed: \"%s\" is an internal function!"			,
+							get_active_function_name(TSRMLS_C),
+							lower_orig,  lower_new, lower_orig);
+		RETVAL_FALSE;
+		goto rename_end;
+	}
+
+	if (zend_hash_find(EG(function_table), lower_new, new_fname_len + 1, (void **) &dummy_func) == SUCCESS) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s(%s, %s) failed: %s already exists!"			,
+							get_active_function_name(TSRMLS_C),
+							lower_orig,  lower_new, lower_new);
+		RETVAL_FALSE;
+		goto rename_end;
+	}
+
+	if (zend_hash_add(EG(function_table), lower_new, new_fname_len + 1, func, sizeof(zend_function), NULL) == FAILURE) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s() failed to insert %s into EG(function_table)", get_active_function_name(TSRMLS_C), lower_new);
+		RETVAL_FALSE;
+		goto rename_end;
+	}
+
+	/* NOTE(cjiang): This line of code is our fix for APD's rename_function. */
+	function_add_ref(func);
+
+	if (zend_hash_del(EG(function_table), lower_orig, orig_fname_len + 1) == FAILURE) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s() failed to remove %s from function table", get_active_function_name(TSRMLS_C), lower_orig);
+
+		zend_hash_del(EG(function_table), lower_new, new_fname_len + 1);
+		RETVAL_FALSE;
+		goto rename_end;
+	}
+	RETVAL_TRUE;
+
+rename_end:
+	free(lower_orig);
+	free(lower_new);
+}
+/* }}} */
+
 /* {{{ arginfo */
 /* {{{ unset_new_overload */
 ZEND_BEGIN_ARG_INFO(arginfo_unset_new_overload, 0)
@@ -256,6 +331,7 @@ ZEND_END_ARG_INFO()
 static const zend_function_entry test_helpers_functions[] = {
 	PHP_FE(unset_new_overload, arginfo_unset_new_overload)
 	PHP_FE(set_new_overload, arginfo_set_new_overload)
+	PHP_FE(rename_function, NULL)
 	{NULL, NULL, NULL}
 };
 /* }}} */
