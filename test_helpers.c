61744fbc49fec7d2bd411d16dfd8061d1fce7cd5
Johannes Schlüter <johannes@schlueters.de>
2010-10-10 00:46:12 +0200

    Clean up code.
    This is the first step for renaming methods. Also drops some bad gotos.

--- test_helpers.c
+++ test_helpers.c
@@ -389,14 +389,58 @@ static PHP_FUNCTION(unset_exit_overload)
 }
 /* }}} */
 
+static int pth_rename_function(HashTable *table, char *orig, int orig_len, char *new, int new_len) /* {{{ */
+{
+	zend_function *func, *dummy_func;
+
+	if (zend_hash_find(table, orig, orig_len + 1, (void **) &func) == FAILURE) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s(%s, %s) failed: %s does not exist!"			,
+						get_active_function_name(TSRMLS_C),
+						orig,  new, orig);
+		return FAILURE;
+	}
+
+	/* TODO: Add infrastructure for resetting internal funcs */
+	if (func->type != ZEND_USER_FUNCTION) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "\"%s\" is an internal function", orig);
+		return FAILURE;
+	}
+
+	if (zend_hash_find(table, new, new_len + 1, (void **) &dummy_func) == SUCCESS) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s(%s, %s) failed: %s already exists!"			,
+							get_active_function_name(TSRMLS_C),
+							orig,  new, new);
+		return FAILURE;
+	}
+
+	if (zend_hash_add(table, new, new_len + 1, func, sizeof(zend_function), NULL) == FAILURE) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s() failed to insert %s into EG(function_table)", get_active_function_name(TSRMLS_C), new);
+		return FAILURE;
+	}
+
+	if (func->type == ZEND_USER_FUNCTION) {
+		function_add_ref(func);
+	}
+
+	if (zend_hash_del(table, orig, orig_len + 1) == FAILURE) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s() failed to remove %s from function table", get_active_function_name(TSRMLS_C), orig);
+
+		zend_hash_del(table, new, new_len + 1);
+		return FAILURE;
+	}
+
+	return SUCCESS;
+}
+/* }}} */
+
 /* {{{ proto bool rename_function(string orig_func_name, string new_func_name)
    Rename a function from its original to a new name. This is mainly useful in
    unittest to stub out untested functions */
 PHP_FUNCTION(rename_function)
 {
-	zend_function *func, *dummy_func;
 	char *orig_fname, *new_fname, *lower_orig, *lower_new;
 	int orig_fname_len, new_fname_len;
+	int success;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "ss", &orig_fname, &orig_fname_len, &new_fname, &new_fname_len) == FAILURE) {
 		return;
@@ -405,51 +449,16 @@ PHP_FUNCTION(rename_function)
 	lower_orig = zend_str_tolower_dup(orig_fname, orig_fname_len);
 	lower_new = zend_str_tolower_dup(new_fname, new_fname_len);
 
-	if (zend_hash_find(EG(function_table), lower_orig, orig_fname_len + 1, (void **) &func) == FAILURE) {
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s(%s, %s) failed: %s does not exist!"			,
-						get_active_function_name(TSRMLS_C),
-						lower_orig,  lower_new, lower_orig);
-		RETVAL_FALSE;
-		goto rename_end;
-	}
+	success = pth_rename_function(EG(function_table), lower_orig, orig_fname_len, lower_new, new_fname_len);
 
-	if (func->type != ZEND_USER_FUNCTION) {
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s(%s, %s) failed: \"%s\" is an internal function!"			,
-							get_active_function_name(TSRMLS_C),
-							lower_orig,  lower_new, lower_orig);
-		RETVAL_FALSE;
-		goto rename_end;
-	}
-
-	if (zend_hash_find(EG(function_table), lower_new, new_fname_len + 1, (void **) &dummy_func) == SUCCESS) {
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s(%s, %s) failed: %s already exists!"			,
-							get_active_function_name(TSRMLS_C),
-							lower_orig,  lower_new, lower_new);
-		RETVAL_FALSE;
-		goto rename_end;
-	}
-
-	if (zend_hash_add(EG(function_table), lower_new, new_fname_len + 1, func, sizeof(zend_function), NULL) == FAILURE) {
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s() failed to insert %s into EG(function_table)", get_active_function_name(TSRMLS_C), lower_new);
-		RETVAL_FALSE;
-		goto rename_end;
-	}
-
-	/* NOTE(cjiang): This line of code is our fix for APD's rename_function. */
-	function_add_ref(func);
-
-	if (zend_hash_del(EG(function_table), lower_orig, orig_fname_len + 1) == FAILURE) {
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s() failed to remove %s from function table", get_active_function_name(TSRMLS_C), lower_orig);
-
-		zend_hash_del(EG(function_table), lower_new, new_fname_len + 1);
-		RETVAL_FALSE;
-		goto rename_end;
-	}
-	RETVAL_TRUE;
-
-rename_end:
 	efree(lower_orig);
 	efree(lower_new);
+
+	if (success == SUCCESS) {
+		RETURN_TRUE;
+	} else {
+		RETURN_FALSE;
+	}
 }
 /* }}} */
 
